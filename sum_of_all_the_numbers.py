b905a38bc1fbbb71d6875c9e2dc69c8e5fb6b5b1
jemmafaye13-hue <jemma.faye13@gmail.com>
2026-03-09 23:46:21 +0800

    add program to calculate the sum of all 10 input numbers

---
+++ sum_of_all_the_numbers.py
@@ -0,0 +1,7 @@
+#Prog07: Create a program that ask user to input 10 numbers. Print the sum of all the numbers.
+
+total = 0
+for i in range(10):
+    num = float(input(f"Enter number {i+1}: "))
+    total += num
+print(f"Total Sum: {total}")
