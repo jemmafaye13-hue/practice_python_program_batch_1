8c9ba314946bac95a3f588c8c6c8e378e6117397
jemmafaye13-hue <jemma.faye13@gmail.com>
2026-03-09 23:56:49 +0800

    add program to count odd numbers from 10 inputs

---
+++ count_odd_numbers.py
@@ -0,0 +1,8 @@
+#Prog08: Create a program that ask user to input 10 numbers. Print how many are odd numbers.
+
+odd_count = 0
+for i in range(10):
+    num = int(input(f"Enter number {i+1}: "))
+    if num % 2 != 0:
+        odd_count += 1
+print(f"Number of odd numbers: {odd_count}")
