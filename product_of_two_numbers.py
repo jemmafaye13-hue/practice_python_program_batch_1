cbc136afc7dfffe5d53179c16396558b101dbe1d
jemmafaye13-hue <jemma.faye13@gmail.com>
2026-03-09 23:15:38 +0800

    add program to calculate the product of two numbers

---
+++ product_of_two_numbers.py
@@ -0,0 +1,5 @@
+#Prog04: Create a program that ask user to input 2 numbers. Print the product of the two numbers.
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+print(f"Product: {num1 * num2}")
+
