6cf0ed7746973e0c443f69ddc093e165166a0fe3
jemmafaye13-hue <jemma.faye13@gmail.com>
2026-03-09 20:50:50 +0800

    rename programs to be more descriptive

--- find_bigger_number.py
+++ find_bigger_number.py
@@ -1,5 +1,4 @@
 #Prog01: Create a program that ask user to input 2 numbers. Print the bigger number.
-#input 1
 num1 = float(input("Enter first Number: "))
 num2 = float(input("Enter second Number: "))
 print(f"The bigger number is: {max(num1, num2)}")
---
+++ program_03.py
@@ -0,0 +1,4 @@
+#Prog03: Create a program that ask user to input 2 numbers. Print the sum of the two numbers.
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+print(f"Sum: {num1 + num2}")
