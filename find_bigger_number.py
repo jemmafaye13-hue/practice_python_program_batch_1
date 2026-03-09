413729440557cb77cf2970e6f621dcd8ebcfa3fe
jemmafaye13-hue <jemma.faye13@gmail.com>
2026-03-09 22:57:19 +0800

    change message input

--- find_bigger_number.py
+++ find_bigger_number.py
@@ -1,6 +1,6 @@
 #Prog01: Create a program that ask user to input 2 numbers. Print the bigger number.
-num1 = float(input("Enter first Number: "))
-num2 = float(input("Enter second Number: "))
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
 print(f"The bigger number is: {max(num1, num2)}")
 
 
